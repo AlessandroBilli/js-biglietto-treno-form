dcf956ea58759694107af3d78f09ee4c5f66b474
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-07 22:57:01 +0100

    feat: risulatato biglietto

--- myscript.js
+++ myscript.js
@@ -6,17 +6,31 @@ const confermaButton = document.getElementById('confermaButton');
 
 confermaButton.addEventListener('click', function () {
     const nome = nomeInput.value;
-    console.log('Nome passeggero', nome)
+    console.log('Nome e Cognome passeggero:', nome)
 
     const età = etàInput.value;
-    console.log ('Età passeggero', età)
+    console.log ('Età passeggero:', età)
 
     const distanza = Number(distanzaInput.value);
-    console.log('Distanza passeggero', distanza);
+    console.log('Distanza passeggero:', distanza);
+
+    document.getElementById('risultatoNome').textContent = 'Nome passeggero: ' + nome;
+    
 
 
     const prezzo = distanza * 0.21;
     console.log('Prezzo del biglietto:', prezzo);
+    document.getElementById('risultatoDistanza').textContent = 'Prezzo: ' + prezzo;
+
+    const numeroCasualeIntero = Math.floor(Math.random() * 10) + 1;
+    console.log(numeroCasualeIntero);
+    document.getElementById('carrozzaNumero').textContent = 'Numero Carrozza: ' + numeroCasualeIntero;
+
+    const numeroCasualeInteroCinqueCifre = Math.floor(Math.random() * 10000) + 10000;
+    console.log(numeroCasualeInteroCinqueCifre);
+    document.getElementById('codiceCP').textContent = 'Numero Carrozza: ' + numeroCasualeInteroCinqueCifre;
+
+    
 
     const percentualeVenti = 20;
 
@@ -28,6 +42,7 @@ confermaButton.addEventListener('click', function () {
         console.log('Sconto del 20%:', sconto);
         const prezzoScontato = prezzo - sconto;
         console.log('Prezzo con lo sconto:', prezzoScontato.toFixed(2));
+        document.getElementById('risultatoDistanza').textContent = 'Prezzo: ' + prezzoScontato.toFixed(2);
     }
 
     if (età > 65) {
@@ -36,10 +51,13 @@ confermaButton.addEventListener('click', function () {
         console.log('Sconto del 40%:', sconto);
         const prezzoScontato = prezzo - sconto;
         console.log('Prezzo con lo sconto:', prezzoScontato.toFixed(2));
+        document.getElementById('risultatoDistanza').textContent = 'Prezzo: ' + prezzoScontato.toFixed(2);
     }
 
 
 
+
+    
 
 
 });
